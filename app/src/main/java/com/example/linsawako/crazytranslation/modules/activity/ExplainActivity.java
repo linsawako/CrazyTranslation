103a425a26b1271f992aa827f4a5dbac6b2aa1fd
linsawako <781428152@qq.com>
2017-02-18 21:32:43 +0800

    添加CrashHandler，使用MD

--- app/src/main/java/com/example/linsawako/crazytranslation/modules/activity/ExplainActivity.java
+++ app/src/main/java/com/example/linsawako/crazytranslation/modules/activity/ExplainActivity.java
@@ -2,9 +2,11 @@ package com.example.linsawako.crazytranslation.modules.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
@@ -17,13 +19,17 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-
 public class ExplainActivity extends AppCompatActivity {
 
-    @BindView(R.id.explain_text)
+    @BindView(R.id.phonetic_cardView)
+    CardView phonetic_cardVew;
+
+    @BindView(R.id.toolbar_layout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    @BindView(R.id.phonetic)
+    TextView phoneticText;
+    @BindView(R.id.explains)
     TextView explainText;
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +39,11 @@ public class ExplainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
 
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         Intent intent = getIntent();
         Word word = (Word)intent.getSerializableExtra("word_data");
         String response;
+
+        collapsingToolbarLayout.setTitle(word.getQuery());
 
         if (word.getBasic() != null){
             StringBuilder stringBuilder = new StringBuilder();
@@ -54,9 +53,13 @@ public class ExplainActivity extends AppCompatActivity {
                 stringBuilder.append(string + "\n");
             }
             response = stringBuilder.toString();
+            explainText.setText(response);
+
+            String phonetic = "US;" + word.getBasic().getUsphonetic() + " UK:" + word.getBasic().getUkphonetic();
+            phoneticText.setText(phonetic);
         }else{
             response = word.getTranslation().get(0);
+            phonetic_cardVew.setVisibility(View.INVISIBLE);
         }
-        explainText.setText(response);
     }
 }
